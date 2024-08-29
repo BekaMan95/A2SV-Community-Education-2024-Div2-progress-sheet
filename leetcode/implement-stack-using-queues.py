c8fd3bab8abf454b6c5c3500c1eb4076e69e64ca
Bereket Sahle <115992290+BekaMan95@users.noreply.github.com>
2024-08-29 12:00:39 +0300

    Create implement-stack-using-queues.py

---
+++ leetcode/implement-stack-using-queues.py
@@ -0,0 +1,16 @@
+class MyStack:
+
+    def __init__(self):
+        self.que = deque()
+
+    def push(self, x: int) -> None:
+        self.que.append(x)
+
+    def pop(self) -> int:
+        return self.que.pop()
+
+    def top(self) -> int:
+        return self.que[-1]
+
+    def empty(self) -> bool:
+        return len(self.que) == 0
